7ccaae0fecb9738f33ad60b5fafd3de1cc80fd61
Ahmed <ahmedabdulrahmanm2001@gmail.com>
2025-09-06 23:13:18 +0200

    adding inline styles

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main>
+            <p style={{fontSize:"50px", color:"white", backgroundColor:"navy"}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
