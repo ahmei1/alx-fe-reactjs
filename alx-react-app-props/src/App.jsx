b863c7778c98efd5cf041e387fda156a573d4322
Ahmed <ahmedabdulrahmanm2001@gmail.com>
2025-09-07 18:48:27 +0200

    creating the usecontext hook and practice it

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,14 @@
+import Profilepage from './components/ProfilePage';
+import UserContext from './components/UserContext';
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <Profilepage  />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
