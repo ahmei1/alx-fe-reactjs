b0cd65966c9a823c56e9304065045e523ce8cc17
Ahmed <ahmedabdulrahmanm2001@gmail.com>
2025-09-07 18:52:31 +0200

    adding the context in the profile jsx

--- alx-react-app-props/src/components/ProfilePage.jsx
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -1,7 +1,10 @@
 import UserInfo from './UserInfo';
+import UserContext from './UserContext';
+import { useContext } from 'react';
 
 function ProfilePage() {
-  return <UserInfo  />;
+    const userData = useContext(UserContext);
+  return <h1>the user: {userData.name}, and the email: {userData.email}</h1>;
 }
 
 export default ProfilePage;
